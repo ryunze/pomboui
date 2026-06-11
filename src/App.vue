c11825638faea792529139184d5aff269cb614d4
ryunze <ryunzeroeight08@gmail.com>
2026-06-11 13:34:38 +0700

    fix: dialog

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
+
+	import { ref } from 'vue'
+
 	import AppBar from '@/components/AppBar.vue'
 	import Card from '@/components/Card.vue'
 	import Button from '@/components/Button.vue'
 	import ButtonFloating from '@/components/ButtonFloating.vue'
 	import Dialog from '@/components/Dialog.vue'
+
+	const dialogVisible = ref(false)
+
+	const handleDialog = () => {
+		dialogVisible.value = true
+	}
+
 </script>
 
 <template>
  	<AppBar/>
  	<main class="mt-15 p-3">
- 		<Dialog/>
+ 		<Dialog v-model="dialogVisible"/>
  		<Card class="mb-6"/>
- 		<Button/>
+ 		<Button @click="handleDialog"/>
  		<ButtonFloating/>
  	</main>
 </template>
--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,6 +1,14 @@
+<script setup>
+	
+	import { ref } from 'vue'
+
+	const visible = defineModel()
+
+</script>
+
 <template>
 	<!-- @ Dialog -->
-	<div class="fixed top-0 bottom-0 left-0 right-0 bg-black/25 flex z-15">
+	<div :class="visible ? 'fixed' : 'hidden'" class="top-0 bottom-0 left-0 right-0 bg-black/25 flex z-15">
 		<div class="w-[84%] p-6 m-auto bg-white shadow-xl/36">
 			<!-- @ Dialog/Title -->
 			<section class="mb-3">
@@ -10,7 +18,7 @@
 			<!-- % Dialog/Title -->
 			<!-- @ Dialog/Button -->
 			<section class="flex">
-				<button class="text-blue-700 ms-auto">Close</button>
+				<button @click="() => { visible = false }" class="text-blue-700 ms-auto">Close</button>
 			</section>
 			<!-- % Dialog/Button -->
 		</div>
